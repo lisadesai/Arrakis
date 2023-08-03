9d75069662d507ec8f7444df7eef58181bf433b2
unknown <lisadesai08@gmail.com>
2023-08-03 21:27:49 +0100

    dynamic pets table displayed

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Pets } from "./components/pets/Pets";
+import Home from './components/Home';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import 'primereact/resources/themes/lara-light-purple/theme.css';
+import 'primereact/resources/primereact.min.css';
+
+
 
 const App = () => {
-  return <Pets />;
+
+  return (
+      <Pets/>     
+  );
+
 };
 
 export default App;
